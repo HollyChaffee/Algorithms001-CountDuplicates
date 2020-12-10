e2a953c2dfadb003fe132f153f8ea283fb28d235
Holly Chaffee <saltdc95@gmail.com>
2020-12-10 12:43:52 -0500

    Count duplicates finished

--- src/main/java/hbcu/stay/ready/algorithms/CountDuplicates.java
+++ src/main/java/hbcu/stay/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,7 @@
 package hbcu.stay.ready.algorithms;
 
+import java.util.Arrays;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,15 +9,39 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
-    }
 
+        Arrays.sort(input);
+
+        Integer duplicates = 0;
+        Object currentVal = null;
+
+        for(int i = 0; i < input.length - 1; i++) {
+            if(input[i] == input[i + 1] && input[i] != currentVal) {
+                duplicates++;
+                currentVal = input[i];
+            }
+        }
+        return duplicates;
+    }
     /**
      * Example of overloading a function that accepts a Array of Integers
      * @param input
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+
+        Arrays.sort(input);
+
+        Integer duplicates = 0;
+        Integer currentVal = null;
+        for (int i = 0; i < input.length - 1; i++) {
+            if (input[i] == input[i + 1] && (!input[i].equals(currentVal))){
+                duplicates++;
+                currentVal = input[i];
+            }
+
+        }
+        return duplicates;
     }
 }
+
